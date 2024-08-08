2822fc5787507d991dcb9484e24161c4c829cfa4
Dalton Pettus <89285609+Daltraxx@users.noreply.github.com>
2024-08-07 18:17:40 -0700

    Add handling for if  user wants to use a field defined in the file or generate a new one

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const prompt = require('prompt-sync')({ sigint: true });
 const { Field } = require('./FieldClass');
-let game = new Field([
+let defineYourOwnField = new Field([
     ['*', '░', 'O'],
     ['░', 'O', '░'],
     ['░', '^', '░'],
 ]);
-let customGame = new Field();
-customGame.playGame();
+let gameStarted = false;
+while (gameStarted === false) {
+    let answer = prompt('Enter "1" to use a Field in this file or "2" to start generating a new field. >> ');
+    switch (answer) {
+        case '1':
+            gameStarted = true;
+            defineYourOwnField.playGame();
+            break;
+        case '2':
+            gameStarted = true;
+            let customGame = new Field();
+            customGame.playGame();
+            break;
+        default:
+            console.log('Invalid entry.');
+    }
+}
--- main.ts
+++ main.ts
@@ -1,14 +1,32 @@
 //to get around ts block-scoped variable bug
 export {};
+const prompt = require('prompt-sync')({sigint: true});
 const { Field } = require('./FieldClass');
 
 
-let game = new Field([
+let defineYourOwnField = new Field([
     ['*', '░', 'O'],
     ['░', 'O', '░'],
     ['░', '^', '░'],
   ]);
 
-let customGame = new Field();
 
-customGame.playGame();
+
+let gameStarted : boolean = false;
+
+while (gameStarted === false) {
+    let answer : string | null = prompt('Enter "1" to use a Field in this file or "2" to start generating a new field. >> ');
+    switch (answer) {
+        case '1':
+            gameStarted = true;
+            defineYourOwnField.playGame();
+            break;
+        case '2':
+            gameStarted = true;
+            let customGame = new Field();
+            customGame.playGame();
+            break;
+        default:
+            console.log('Invalid entry.');
+    }
+}
