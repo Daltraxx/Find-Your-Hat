61a62a8c97dbd5bdfe79a4a3bbf2ae2ce905fd82
Dalton Pettus <daltpettus@gmail.com>
2025-09-22 14:22:15 -0700

    remove obsolete files

--- ensureSolvableAlgorithm/getPositionString.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var getPositionString = function (position) {
-    var positionString = "".concat(position[0], "-").concat(position[1]);
-    return positionString;
-};
-exports.default = getPositionString;
