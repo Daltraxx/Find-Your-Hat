7276f6460b50325409df61c380ae405491302841
Dalton Pettus <daltpettus@gmail.com>
2025-09-22 13:59:34 -0700

    fix dfs algorithm determining solvability

--- ensureSolvableAlgorithm/isGameGridSolvable.js
+++ ensureSolvableAlgorithm/isGameGridSolvable.js
@@ -1,20 +1,55 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const mapGameGrid_js_1 = require("./mapGameGrid.js");
-const getPositionString_js_1 = require("./getPositionString.js");
-const findPathToHat_js_1 = require("./findPathToHat.js");
-const isGameGridSolvable = (gameGrid, playerPosition) => {
-    const gameGraph = (0, mapGameGrid_js_1.default)(gameGrid);
-    const startingPoint = gameGraph.getVertexByValue((0, getPositionString_js_1.default)(playerPosition));
-    return (0, findPathToHat_js_1.default)(startingPoint, gameGrid);
+var FieldClass_js_1 = require("../FieldClass.js");
+var isGameGridSolvable = function (gameGrid, playerPosition) {
+    var hashPosition = function (position) {
+        var row = position[0], col = position[1];
+        return "".concat(row, "-").concat(col);
+    };
+    var isValidPosition = function (row, col) {
+        return (row >= 0 && row < gameGrid.length && col >= 0 && col < gameGrid[0].length);
+    };
+    var directions = [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ];
+    var seen = [hashPosition(playerPosition)];
+    var stack = [playerPosition];
+    while (stack.length) {
+        var position = stack.pop();
+        if (!position)
+            continue;
+        var rowPosition = position[0], colPosition = position[1];
+        for (var _i = 0, directions_1 = directions; _i < directions_1.length; _i++) {
+            var _a = directions_1[_i], xMovement = _a[0], yMovement = _a[1];
+            var newRowPosition = rowPosition + yMovement;
+            var newColPosition = colPosition + xMovement;
+            var fullPosition = [newRowPosition, newColPosition];
+            if (isValidPosition(newRowPosition, newColPosition) &&
+                !seen.includes(hashPosition(fullPosition))) {
+                var positionCharacter = gameGrid[newRowPosition][newColPosition];
+                if (positionCharacter === FieldClass_js_1.Character.Hat)
+                    return true;
+                var hashedPosition = hashPosition(fullPosition);
+                if (positionCharacter === FieldClass_js_1.Character.Hole) {
+                    seen.push(hashedPosition);
+                }
+                else {
+                    stack.push(fullPosition);
+                    seen.push(hashedPosition);
+                }
+            }
+        }
+    }
+    return false;
 };
-/* For testing
-const testField = [
-    ['!', 'O', 'O'],
-    ['░', '░', '░'],
-    ['O', '^', '░'],
-]
-const playerPosition = [0, 0];
-console.log(isGameGridSolvable(testField, playerPosition));
-*/
+// const testField = [
+//   ["!", "O", "^"],
+//   ["░", "░", "░"],
+//   ["O", "O", "░"],
+// ];
+// const playerPosition: [number, number] = [0, 0];
+// console.log(isGameGridSolvable(testField, playerPosition));
 module.exports.isGameGridSolvable = isGameGridSolvable;
--- ensureSolvableAlgorithm/isGameGridSolvable.ts
+++ ensureSolvableAlgorithm/isGameGridSolvable.ts
@@ -1,23 +1,63 @@
 import mapGameGrid from "./mapGameGrid.js";
 import getPositionString from "./getPositionString.js";
 import findPathToHat from "./findPathToHat.js";
+import { Character } from "../FieldClass.js";
 
+const isGameGridSolvable = (
+  gameGrid: string[][],
+  playerPosition: [number, number]
+) => {
+  const hashPosition = (position: [number, number]) => {
+    const [row, col] = position;
+    return `${row}-${col}`;
+  };
+  const isValidPosition = (row, col) => {
+    return (
+      row >= 0 && row < gameGrid.length && col >= 0 && col < gameGrid[0].length
+    );
+  };
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+  const seen = [hashPosition(playerPosition)];
+  const stack = [playerPosition];
+  while (stack.length) {
+    const position = stack.pop();
+    if (!position) continue;
+    let [rowPosition, colPosition] = position;
+    for (let [xMovement, yMovement] of directions) {
+      const newRowPosition = rowPosition + yMovement;
+      const newColPosition = colPosition + xMovement;
+      const fullPosition: [number, number] = [newRowPosition, newColPosition];
+      if (
+        isValidPosition(newRowPosition, newColPosition) &&
+        !seen.includes(hashPosition(fullPosition))
+      ) {
+        const positionCharacter = gameGrid[newRowPosition][newColPosition];
+        if (positionCharacter === Character.Hat) return true;
+        const hashedPosition = hashPosition(fullPosition);
+        if (positionCharacter === Character.Hole) {
+          seen.push(hashedPosition);
+        } else {
+          stack.push(fullPosition);
+          seen.push(hashedPosition);
+        }
+      }
+    }
+  }
 
-const isGameGridSolvable = (gameGrid, playerPosition) => {
-    const gameGraph = mapGameGrid(gameGrid);
-    const startingPoint = gameGraph.getVertexByValue(getPositionString(playerPosition));
+  return false;
+};
 
-    return findPathToHat(startingPoint, gameGrid);
-}
-
-/* For testing
-const testField = [
-    ['!', 'O', 'O'],
-    ['░', '░', '░'],
-    ['O', '^', '░'],
-]
-const playerPosition = [0, 0];
-console.log(isGameGridSolvable(testField, playerPosition));
-*/
+// const testField = [
+//   ["!", "O", "^"],
+//   ["░", "░", "░"],
+//   ["O", "O", "░"],
+// ];
+// const playerPosition: [number, number] = [0, 0];
+// console.log(isGameGridSolvable(testField, playerPosition));
 
 module.exports.isGameGridSolvable = isGameGridSolvable;
